27af303531b5a5913ed3f8353fefdbeabc2e8307
Davasny <davasny@gmail.com>
2026-04-02 11:53:55 +0200

    chore: use pino-pretty logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,17 +1,27 @@
 import pino from "pino"
+import pretty from "pino-pretty"
 
-export const logger = pino({
-  base: {
-    service: "project-intern",
-  },
-  level: process.env.NODE_ENV === "development" ? "debug" : "info",
-  redact: {
-    paths: [
-      "payload.token",
-      "payload.authorization",
-      "req.headers.authorization",
-    ],
-    remove: true,
-  },
-  timestamp: pino.stdTimeFunctions.isoTime,
+const prettyStream = pretty({
+  colorize: true,
+  ignore: "pid,hostname",
+  singleLine: true,
 })
+
+export const logger = pino(
+  {
+    base: {
+      service: "project-intern",
+    },
+    level: process.env.NODE_ENV === "development" ? "debug" : "info",
+    redact: {
+      paths: [
+        "payload.token",
+        "payload.authorization",
+        "req.headers.authorization",
+      ],
+      remove: true,
+    },
+    timestamp: pino.stdTimeFunctions.isoTime,
+  },
+  prettyStream,
+)
